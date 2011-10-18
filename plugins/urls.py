8e0201054340576b858ec2a8443d320df3768f51
Julian Wachholz <me@julianwachholz.ch>
2011-10-18 21:13:17 +0200

    now shebang urls are parsed correctly

--- plugins/urls.py
+++ plugins/urls.py
@@ -5,10 +5,12 @@ ignore = ['buttbot']
 
 @hook.regex(r'([a-zA-Z]+://|www\.)[^ ]+')
 def show_title(match, nick='', chan='', say=None):
-    url = urlnorm.normalize(match.group().encode('utf-8'))
-    if "#!" in url:
-        url = url.replace("#!", "&_escaped_fragment_=")
+    url = match.group().encode('utf-8')
+    if "#!/" in url:
+        url = url.replace("#!/", "&_escaped_fragment_=")
+    url = urlnorm.normalize(url)
     if not url in ignore and not nick in ignore:
+        print 'scraping '+url+'\n'
         page = http.get_html(url)
         title = page.xpath('//title')
         if title and 'youtube' not in url:
