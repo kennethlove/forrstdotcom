43bbf5da3ebe57719194ca9e2a477835b62c02ca
Yorick Peterse <yorickpeterse@gmail.com>
2011-11-23 00:53:47 +0100

    Show URL content type and shorten the URL.
    Whenever a user pastes a URL into a channel the bot will now show not only the
    title but also the content type and a short URL using is.gd. The title is only
    shown when found, the other two elements are always displayed.
    
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- plugins/urls.py
+++ plugins/urls.py
@@ -2,19 +2,38 @@ import re
 from util import hook, urlnorm, http
 
 ignore = ['buttbot']
+is_gd  = 'http://is.gd/create.php?format=simple&url=%s'
 
 @hook.regex(r'([a-zA-Z]+://|www\.)[^ ]+')
 def show_title(match, nick='', chan='', say=None):
-    url = urlnorm.normalize(match.group().encode('utf-8'))
+    url     = urlnorm.normalize(match.group().encode('utf-8'))
+
     if not url in ignore and not nick in ignore:
-        page = http.get_html(url)
-        title = page.xpath('//title')
-        if title and 'youtube' not in url and 'twitter' not in url:
-            titleList = []
-            for i in title:
-                if i.text_content():
-                    titleList.append(i.text_content().strip())
-            if titleList:
-                titleList = ''.join(titleList)
-                string = "URL title: %s" % (''.join(titleList))
-                say(string)
+        page, response = http.get_html_and_response(url)
+        title          = page.xpath('//title')
+        message        = 'Content type: %s' % (response.headers.gettype())
+
+        # Only ignore URLs of which "twitter" or "youtube" is part of the
+        # domain and not just part some some URI segment.
+        if 'youtube.' not in url and 'twitter.' not in url:
+            # Don't show the title if there isn't one
+            if title:
+                titleList = []
+                short_url = 'Not Found'
+
+                for i in title:
+                    if i.text_content():
+                        titleList.append(i.text_content().strip())
+
+                if titleList:
+                    titleList = ''.join(titleList)
+                    message   += ' | URL title: %s' % (''.join(titleList))
+
+        # Get the short URL.
+        short_url = http.get(is_gd % (url))
+
+        # Cheap error checking
+        if 'error: please' not in short_url.lower():
+            message += ' | Short URL: %s' % (short_url)
+
+        say(message)
